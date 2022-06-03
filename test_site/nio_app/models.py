3c2dbf1eef47f142267fc8fbdd787b75b0a9d4f0
VIVOG <vivog2017@gmail.com>
2022-06-03 12:09:59 +0300

    added some models makemigration migrate soo create tables for database

--- test_site/nio_app/models.py
+++ test_site/nio_app/models.py
@@ -6,8 +6,9 @@ from django.urls import reverse
 class Department(models.Model):
     department_name = models.CharField(max_length=30, help_text='не более 30 символов',
                                        verbose_name='Название отдела')
-    fio_chief = models.ManyToManyField('Staff', verbose_name='ФИО')
-    tabel = models.ForeignKey('Staff', on_delete=models.CASCADE, verbose_name='Табельный номер')
+    fio = models.ForeignKey('Staff', on_delete=models.CASCADE, verbose_name='ФИО', related_name='department_fio')
+    tabel = models.ForeignKey('Staff', on_delete=models.CASCADE, verbose_name='Табельный номер',
+                              related_name='department_tabel')
 
     def __str__(self):
         return self.department_name
@@ -16,8 +17,9 @@ class Department(models.Model):
 class Divisions(models.Model):
     division_name = models.CharField(max_length=50, help_text='не более 50 символов',
                                      verbose_name='Название подразделение')
-    fio_chief = models.ManyToManyField('Staff', verbose_name='ФИО')
-    tabel = models.ForeignKey('Staff', on_delete=models.CASCADE, verbose_name='Табельный номер')
+    fio = models.ForeignKey('Staff', on_delete=models.CASCADE, verbose_name='ФИО', related_name='division_fio')
+    tabel = models.ForeignKey('Staff', on_delete=models.CASCADE, verbose_name='Табельный номер',
+                              related_name='division_tabel')
 
     def __str__(self):
         return self.division_name
@@ -39,17 +41,43 @@ class Staff(models.Model):
         return reverse('stuff-detail', args=[str(self.id)])
 
 
+class Condition(models.Model):
+    CHOICES_COND = (
+        ("8", "Полный день"), ("7", "Полный день"), ("Б", "Больничный"), ("О", "Отпуск"), ("Н", "Неизвестная причина"))
+    cond_status = models.CharField(max_length=5, help_text="Выберите один из вариантов",
+                                   verbose_name="Сведения присутствия", choices=CHOICES_COND)
+
+    def __str__(self):
+        return self.cond_status
+
+
+class Timesheet(models.Model):
+    fio = models.ForeignKey('Staff', on_delete=models.CASCADE, verbose_name='ФИО', related_name='timesheet_fio')
+    tabel = models.ForeignKey('Staff', on_delete=models.CASCADE, verbose_name='Табельный номер',
+                              related_name='timesheet_tabel')
+    condition = models.ForeignKey(Condition, on_delete=models.CASCADE, verbose_name="Сведения присутствия")
+    date = models.DateField(help_text='Выберите дату', verbose_name='Дата табелирования')
+
+    class Meta:
+        ordering = ['date']
+
+    def __str__(self):
+        return f"{self.date} {self.tabel} {self.fio} {self.condition}"
+
+
 class Documents(models.Model):
     doc_name = models.CharField(max_length=200, help_text="Введите название документа",
                                 verbose_name="Название документа")
-    author = models.ManyToManyField('Staff', verbose_name='ФИО', help_text="Введите автора документа")
+    author = models.ForeignKey('Staff', on_delete=models.CASCADE, verbose_name='ФИО',
+                               help_text="Введите автора документа")
 
-    CHOICES_DOC_TYPE = ["Методика", "Паспорт", "Руководство по эксплуатации", "Техническая справка",
-                        "Технический отчет", "Технологическая инструкция", "Другое"]
+    CHOICES_DOC_TYPE = (
+        ("М", "Методика"), ("П", "Паспорт"), ("РЭ", "Руководство по эксплуатации"), ("ТС", "Техническая справка"),
+        ("ОТ", "Технический отчет"), ("ТИ", "Технологическая инструкция"), ("Д", "Другое"),)
     doc_type = models.CharField(max_length=50, help_text="Введите тип документа",
                                 verbose_name="Название документа", choices=CHOICES_DOC_TYPE)
 
-    CHOICES_STATUS = ["Разработка", "Согласование", "Внедрено"]
+    CHOICES_STATUS = (("Р", "Разработка"), ("С", "Согласование"), ("В", "Внедрено"))
     doc_status = models.CharField(max_length=50, help_text="Текущий статус документа",
                                   verbose_name="Статус документа", choices=CHOICES_STATUS)
 
