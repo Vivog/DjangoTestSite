5a03223aa7a0973719e3287e85409ab8af655004
vivgg <vivog2017@gmail.com>
2022-08-02 12:56:22 +0300

    so me

--- test_site/nio_app/views.py
+++ test_site/nio_app/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import logout, login
 from django.contrib.auth.views import LoginView
-from django.http import HttpResponseNotFound
+from django.http import HttpResponseNotFound, HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.utils.text import slugify
@@ -23,6 +23,16 @@ CONTEXT['projects'] = Projects.objects.all()
 CONTEXT['publications'] = Publications.objects.all()
 CONTEXT['news'] = News.objects.all()
 CONTEXT['staff'] = Staff.objects.all()
+STAFF = Staff.objects.all()
+PROF = [('Усі',len(STAFF)), ]
+for s in STAFF:
+    if s.prof not in PROF:
+        num = len(Staff.objects.filter(prof=s.prof))
+        PROF.append((s.prof, num))
+    else:
+        continue
+PROFS = sorted(set(PROF))
+CONTEXT['staff_prof'] = PROFS
 
 
 def index_portal(request):
@@ -41,6 +51,10 @@ class DivisionList(DetailView):
         context['div'] = Divisions.objects.get(slug=self.get_object().slug)
         return context
 
+
+
+
+
 class StaffList(ListView):
     model = Staff
     template_name = 'nio_app/staff.html'
@@ -50,42 +64,54 @@ class StaffList(ListView):
         context = super().get_context_data(object_list=None, **kwargs)
         context.update(CONTEXT)
         context['staff_all'] = Staff.objects.all().count()
-        staff = [('Усі',len(Staff.objects.all())), ]
-        for s in Staff.objects.all():
-            if s.prof not in staff:
-                num = len(Staff.objects.filter(prof=s.prof))
-                staff.append((s.prof, num))
-            else:
-                continue
-        staff = set(staff)
-        context['staff_prof'] = staff
-        # context['form'] = ChoiseStaffForm()
-        # print(context['staff'])
         return context
 
     def post(self, request, *args, **kwargs):
-        if self.request.POST.get('divs'):
-            div = Divisions.objects.filter(abr=self.request.POST.get('divs'))
-            ch = self.request.POST.get('divs')
-            print('CH  ',ch)
-            CONTEXT['staff'] = Staff.objects.filter(div_id=div[0])
-            return render(request, 'nio_app/staff.html', context=CONTEXT)
-        elif self.request.POST.get('prof'):
-            staff = Staff.objects.filter(prof=self.request.POST.get('prof'))
-            CONTEXT['staff_prof'] = Staff.objects.filter(prof=staff[0])
+        if self.request.method == 'POST':
+            print('PROFF ', self.request.POST.get('prof'))
+            print('DIVS ', self.request.POST.get('divs'))
+            sort = self.sort_by(self.request, divs=self.request.POST.get('divs'), prof=self.request.POST.get('prof'))
+            CONTEXT['staff'] = sort
+            # CONTEXT['staff'] = sort[0]
+            # CONTEXT['staff_prof'] = sort[1]
             return render(request, 'nio_app/staff.html', context=CONTEXT)
         else:
-            CONTEXT['staff'] = Staff.objects.all()
-            staff = [('Усі', len(Staff.objects.all())), ]
-            for s in Staff.objects.all():
-                if s.prof not in staff:
-                    num = len(Staff.objects.filter(prof=s.prof))
-                    staff.append((s.prof, num))
-                else:
-                    continue
-            staff = set(staff)
-            CONTEXT['staff_prof'] = staff
+            sort = self.sort_by(self.request)
+            CONTEXT['staff'] = sort[0]
+            # CONTEXT['staff_prof'] = sort[1]
             return render(request, 'nio_app/staff.html', context=CONTEXT)
+
+    def sort_by(self, request, divs=None, prof=None):
+        if divs == None and prof == None:
+            STAFF = Staff.objects.all()
+            # PROF = [('Усі', len(STAFF)), ]
+            # PROF += [(s.prof, len(STAFF.filter(prof=s.prof))) for s in STAFF if s.prof not in PROF]
+            # PROFS = sorted(set(PROF))
+            # return (STAFF, PROFS)
+            return STAFF
+        else:
+            if prof == None and divs != None:
+                div = Divisions.objects.filter(abr=divs)
+                STAFF = Staff.objects.filter(div_id=div[0])
+                # PROF = [('Усі', len(STAFF)), ]
+                # PROF += [(s.prof, len(STAFF.filter(prof=s.prof))) for s in STAFF if s.prof not in PROF]
+                # PROFS = sorted(set(PROF))
+                # return (STAFF, PROFS)
+                return STAFF
+            elif divs == None and prof != None:
+                STAFF = Staff.objects.filter(prof=prof)
+                # PROF = [('Усі', len(STAFF)), ]
+                # PROF += [(s.prof, len(STAFF.filter(prof=s.prof))) for s in STAFF if s.prof not in PROF]
+                # PROFS = sorted(set(PROF))
+                # return (STAFF, PROFS)
+                return STAFF
+            elif divs != None and prof != None:
+                div = Divisions.objects.filter(abr=divs)
+                STAFF = Staff.objects.filter(div_id=div[0]).filter(prof=prof)
+                # PROF = [('Усі', len(STAFF)), (STAFF[0].prof, len(STAFF))]
+                # PROFS = sorted(set(PROF))
+                # return (STAFF, PROFS)
+                return STAFF
 
 
     # def post(self, request):
